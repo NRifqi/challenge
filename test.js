e6d8a6bb4500ce734d5d880521d2b8525e3ecfa4
Nur Rifqi I <islaminurrifqi@gmail.com>
2023-12-23 19:01:13 +0700

    new file

---
+++ test.js
@@ -0,0 +1,110 @@
+// 1. var const let
+// name = "adit"
+// var name
+// console.log(name)
+
+// var name ;
+// console.log(name)
+// name= "adi";
+// console.log(name)
+// name = "adinda"
+// console.log(name)
+// var name = "adidas"
+// console.log( name)
+
+
+// let name ;
+// console.log(name)
+// name = "adi";
+// console.log(name)
+// name = "adinda"
+// console.log(name)
+// let name = "adidas"
+// console.log( name)
+
+// const name = "adi";
+// console.log(name)
+// name = "adinda";
+// console.log(name)
+// const name = "adidas";
+// console.log( name)
+
+// 2. type data
+// let nama = "adi";
+// let hobi = "sepakbola";
+
+// console.log("nama saya", nama, "dan hobi saya", hobi)
+
+// let gaji   = 5000000;
+// let hutang = 400000;
+// let sisa   = gaji - hutang
+// console.log("gaji saya adalah Rp.", gaji, "sedangkan hutang saya Rp.", hutang, "jadi sisa uang yang saya miliki sekarang adalah Rp.", sisa)
+
+// let a = 2;
+//     b = 3;
+//     bool1 = a > b;
+//     bool2 = a < b;
+//     bool3 = a == b;
+// console.log(bool1,bool2,bool3)
+
+// 3. manipulasi string
+// let t = "qwerlgjlkfdlnmhjshfakjdh";
+// console.log(t.length);
+
+// let teks = "halo dek"
+// console.log(teks.slice(0,4))
+
+// 4. kondisi
+// let tabungan = prompt("berapa tabunganmu sekarang?")
+
+// if (tabungan >= 100000000){
+//     alert("pinjem dong")
+// } else if (tabungan > 10000000 && tabungan < 100000000){
+//     alert("keren")
+// } else {
+//     alert("sabar ya")
+// }
+
+// let nilai = 45;
+// let pesan = nilai > 75 ? "selamat anda lulus" : "sabar ya, semangat!!!"
+
+// console.log(pesan)
+
+// 5. data object
+// let idt = {
+//     nama : "adi",
+//     umur : 29,
+//     status : "single"
+// }
+// console.log(idt)
+// console.log(idt.status)
+
+// 6. data array
+// let nama = ["adi", "adit", "adinda"]
+// console.log(nama[0])
+
+// 7. array object
+// let idt2 = [
+//     {
+//         nama : "adi",
+//         umur : 29,
+//         status : "belum menikah"
+//     },
+
+//     {
+//         nama : "adit",
+//         umur : 31,
+//         status : "menikah"
+//     },
+
+// ]
+// console.log(idt2)
+// console.log(idt2[1].status)
+
+//8. function parameter
+// function halo(nama, umur) {
+//     let idt = "hi, nama saya " + nama + " umur saya " + umur;
+//     console.log(idt);
+// }
+
+// halo("adi", 29)
